c040f10c4098ead3705ffaa2b157f6d752e0a24f
Yeabsira Driba <72541965+dryeab@users.noreply.github.com>
2021-12-15 02:23:05 -0500

    Update domino-piling.py

--- domino-piling.py
+++ domino-piling.py
@@ -1,12 +1,5 @@
 # link - https://codeforces.com/problemset/problem/50/A
 
 def solution(m, n):
-    def wrapper(m,n):
-        if not(m%2 or n%2):
-            return (m*n)/2
-        x = m-1
-        return (x*n)/2 + n//2
-    return int(wrapper(m,n))
-
-print(solution(5,5))
+    return (m*n)//2
     
