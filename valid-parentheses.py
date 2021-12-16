ed696619b227a85ea0104cd52d15660bdaa6742a
Yeabsira Driba <72541965+dryeab@users.noreply.github.com>
2021-12-16 12:26:20 -0500

    Create valid-parentheses.py

---
+++ valid-parentheses.py
@@ -0,0 +1,25 @@
+
+# link - https://leetcode.com/problems/valid-parentheses
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        l = []
+        
+        match = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+    
+        if s[0] in match: return False
+        
+        for i in s:
+            if i not in match:
+                l.append(i)
+            else:
+                if len(l) == 0:
+                    return False
+                if match[i] != l.pop():
+                    return False
+        
+        return len(l) == 0
