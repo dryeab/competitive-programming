6ebefc192cee0f8042a30795689f10f8b64f5fa3
Yeabsira Driba <72541965+dryeab@users.noreply.github.com>
2021-12-31 13:46:57 -0500

    Create 779. K-th Symbol in Grammar.py

---
+++ 779. K-th Symbol in Grammar.py
@@ -0,0 +1,14 @@
+
+# link - https://leetcode.com/problems/k-th-symbol-in-grammar/
+
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+
+        def helper(n, k):
+            if n == 1: return 0
+
+            val = helper(n-1, k//2)
+
+            return val if k % 2 == 0 else (1 if val == 0 else 0)
+        
+        return helper(n, k-1)
