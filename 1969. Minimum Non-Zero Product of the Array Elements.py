b67bf6ae9127073b571e71494c5e9955003a8052
Yeabsira Driba <72541965+dryeab@users.noreply.github.com>
2022-01-06 03:53:56 -0500

    Create 1969. Minimum Non-Zero Product of the Array Elements.py

---
+++ 1969. Minimum Non-Zero Product of the Array Elements.py
@@ -0,0 +1,10 @@
+# link - https://leetcode.com/problems/minimum-non-zero-product-of-the-array-elements/
+
+# space: O(1)
+# time: O(1)
+
+class Solution:
+    def minNonZeroProduct(self, p: int) -> int:
+        last = 2**p -1
+        m = 10**9 + 7
+        return (pow(last-1, last//2, m) * last)%m
