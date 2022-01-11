1dd9744c28fbf42e330532cf7d01fbe1be54590d
Yeabsira Driba <72541965+dryeab@users.noreply.github.com>
2022-01-11 09:39:44 -0500

    Update 50. Pow(x, n).py

--- 50. Pow(x, n).py
+++ 50. Pow(x, n).py
@@ -1,14 +1,15 @@
 
 # link - https://leetcode.com/problems/powx-n/
 
+# space: O(1)
+# time: O(log(n))
+
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         if n == 0:
             return 1
         if n < 0:
-            return pow(1/x, -n)
+            return self.myPow(1/x, -n)
         if n % 2 == 0:
-            return  pow(x, n//2) ** 2
-        return x * pow(x, n-1)
-        
-        
+            return  self.myPow(x, n//2) ** 2
+        return x * self.myPow(x, n-1)
