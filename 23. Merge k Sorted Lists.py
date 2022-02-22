3992127669305484b9db940788a339a7b9301500
Yeabsira <72541965+dryeab@users.noreply.github.com>
2022-02-22 07:38:13 -0500

    Update 23. Merge k Sorted Lists.py

--- 23. Merge k Sorted Lists.py
+++ 23. Merge k Sorted Lists.py
@@ -1,6 +1,34 @@
 
 # link - https://leetcode.com/problems/merge-k-sorted-lists/
 
+# solution 1
+
+import heapq
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        
+        ans = []
+        
+        for lst in lists:
+            while lst:
+                heapq.heappush(ans, lst.val)
+                lst = lst.next
+        
+        head = tail = None
+        
+        while ans:
+            node = ListNode(heapq.heappop(ans))
+            if head == None:
+                head = tail = node
+            else:
+                tail.next = node
+                tail = node
+            
+        return head
+
+    
+# solution 2
 
 def counter(lists):
     count = {}
