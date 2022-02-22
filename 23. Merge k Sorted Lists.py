c72d7e3e311fa4683845ed9327e0624e13d071c7
Yeabsira <72541965+dryeab@users.noreply.github.com>
2022-02-22 08:01:47 -0500

    Update 23. Merge k Sorted Lists.py

--- 23. Merge k Sorted Lists.py
+++ 23. Merge k Sorted Lists.py
@@ -6,8 +6,6 @@
 # space: O(n)
 # time: O(nlog(n))
 
-import heapq
-
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         
@@ -28,39 +26,4 @@ class Solution:
                 tail.next = node
                 tail = node
             
-        return head
-
-    
-# solution 2
-
-def counter(lists):
-    count = {}
-    for list in lists:
-        
-        while list:
-            if list.val in count:
-                count[list.val] += 1
-            else:
-                count[list.val] = 1
-            list = list.next
-            
-    return count
-
-class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
-        head = tail = None
-        
-        count = counter(lists)
-        
-        for i in range(-10**4, 10**4+1):
-            while count.get(i, 0):
-                if head == None:
-                    head = tail = ListNode(i)
-                else:
-                    tail.next = ListNode(i)
-                    tail = tail.next
-                    
-                count[i] -= 1
-        
         return head
