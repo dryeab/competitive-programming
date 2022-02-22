af363058ce29486f33833842498defd5e8001b72
Yeabsira <72541965+dryeab@users.noreply.github.com>
2022-02-22 14:37:08 -0500

    Create 1464. Maximum Product of Two Elements in an Array.py

---
+++ 1464. Maximum Product of Two Elements in an Array.py
@@ -0,0 +1,34 @@
+# link - https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+
+
+# solution 1
+
+# space: O(1)
+# time: O(n)
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        
+        x, y = float('-inf'), float('-inf')
+        
+        for num in nums:
+            if num >= y:
+                x, y = y, num
+            elif num > x:
+                x = num
+                
+        return (x-1)*(y-1)
+
+      
+      
+# solution 2
+
+# space: O(1)
+# time: O(nlog(n))
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        return (nums[-1]-1)*(nums[-2]-1)
+
+      
