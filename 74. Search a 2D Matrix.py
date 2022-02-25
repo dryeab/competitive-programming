f00a2daf80e94f68964f74c65325272f2afb881c
Yeabsira <72541965+dryeab@users.noreply.github.com>
2022-02-25 08:54:50 -0500

    Update 74. Search a 2D Matrix.py

--- 74. Search a 2D Matrix.py
+++ 74. Search a 2D Matrix.py
@@ -12,3 +12,35 @@ class Solution:
                 if col == target:
                     return True
         return False
+
+# Solution 2
+  # Space: O(1)
+  # Time: O(m*log(n))
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        # binary search
+        def search(arr, target):
+            
+            left, right = 0, len(arr) - 1
+            
+            while left <= right:
+                
+                mid = (left + right)//2
+                
+                if arr[mid] == target:
+                    return True
+                
+                if arr[mid] > target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+                    
+            return False
+        
+        for row in matrix:
+            if search(row, target):
+                return True
+            
+        return False
