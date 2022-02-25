04ea72e61924d232b091b45cc89e2bcfb259cd81
Yeabsira <72541965+dryeab@users.noreply.github.com>
2022-02-25 13:30:00 -0500

    Update 1170. Compare Strings by Frequency of the Smallest Character.py

--- 1170. Compare Strings by Frequency of the Smallest Character.py
+++ 1170. Compare Strings by Frequency of the Smallest Character.py
@@ -3,6 +3,41 @@
 
 # Solution 1
   # Space: O(1)
+  # Time: ~
+
+class Solution:
+    def numSmallerByFrequency(self, queries: List[str], words: List[str]) -> List[int]:
+        
+        def f(word):
+            return word.count(min(word))
+        
+        for i, word in enumerate(words):
+            words[i] = f(word)
+        
+        words.sort()
+        
+        ans = []
+        for query in queries:
+            
+            q = f(query)
+            
+            left, right = 0, len(words) - 1
+            while left < right:
+                mid = (left + right)//2
+                if words[mid] > q:
+                    right = mid
+                else:
+                    left = mid + 1
+                    
+            if (left == len(words) - 1) and (q >= words[left]):
+                left += 1
+                
+            ans.append(len(words) - left)
+            
+        return ans
+
+# Solution 2
+  # Space: O(1)
   # Time: O(n*m)
 
 class Solution:
