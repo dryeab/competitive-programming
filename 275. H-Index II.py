fc8a5912438e251773c16429099fd22b973dbc69
Yeabsira Driba <dryeab@gmail.com>
2022-02-27 04:39:41 -0500

    heap and binary search

---
+++ 275. H-Index II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        
+        if not citations: return 0
+        
+        left, right = 0, len(citations) - 1
+        while left <= right:
+            mid = (left + right)//2
+            if mid + citations[mid] >= len(citations):
+                right = mid - 1
+            else:
+                left = mid + 1                
+        return len(citations) - left
