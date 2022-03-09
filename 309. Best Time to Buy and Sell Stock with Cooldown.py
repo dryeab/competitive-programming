59f002a55e69f16f751d1dee021b9d19c1e5d9e5
Yeabsira Driba <dryeab@gmail.com>
2022-03-09 05:59:30 -0500

    309. Best Time to Buy and Sell Stock with Cooldown

--- 309. Best Time to Buy and Sell Stock with Cooldown.py
+++ 309. Best Time to Buy and Sell Stock with Cooldown.py
@@ -2,6 +2,28 @@
 # Link - https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
 
 # Space: O(n)
+# Time: O(n)
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        # j - index of min(value(i))
+        j, dp = 0, [0]*len(prices)
+        
+        value = lambda i: (dp[i-2] if i > 1 else 0) - prices[i]
+        
+        for i in range(len(prices)):
+
+            dp[i] = max(
+                value(j) + prices[i], # buy at j and sell at i
+                (0 if i == 0 else dp[i-1]) # treat i as cooldown
+            )
+            
+            if value(i) > value(j): j = i
+        
+        return max(dp)
+
+# Space: O(n)
 # Time: O(n^2)
 
 class Solution:
