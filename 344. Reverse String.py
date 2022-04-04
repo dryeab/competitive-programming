f741be49bde224b18ff1f8a4b1299d82d727aec8
Yeabsira <dryeab@gmail.com>
2022-04-04 14:13:27 +0300

    daily

--- 344. Reverse String.py
+++ 344. Reverse String.py
@@ -6,10 +6,4 @@
 
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-
-        i, j = 0, len(s) - 1
-        while (i < j):
-            s[i], s[j] = s[j], s[i]
-            i += 1
-            j -= 1
-        
+        s[:] = s[::-1]
