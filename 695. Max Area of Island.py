1d9755ef8f35aaca1d0ebcf6f3a1e8ec75cad653
Yeabsira <dryeab@gmail.com>
2022-04-17 14:03:19 +0300

    a2sv

--- 695. Max Area of Island.py
+++ 695. Max Area of Island.py
@@ -30,3 +30,44 @@ class Solution:
                     answer = max(answer, calculateArea(r, c))
         
         return answer
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        
+        m, n = len(grid), len(grid[0])
+        
+        parent = {(i, j): (i, j) for i in range(m) for j in range(n)}
+        size = {(i, j): grid[i][j] for i in range(m) for j in range(n)}
+        
+        def find(v):
+            if v == parent[v]:
+                return v
+            parent[v] = find(parent[v])
+            return parent[v]
+        
+        def union(v1, v2):
+            
+            root1, root2 = find(v1), find(v2)
+            
+            if root1 == root2: return
+            
+            if size[root1] < size[root2]:
+                parent[root1] = root2
+                size[root2] += size[root1]
+            else:
+                parent[root2] = root1
+                size[root1] += size[root2]
+        
+        inBound = lambda r, c: 0 <= r < m and 0 <= c < n
+        DIR = [(0, -1), (-1, 0)] # left, top
+        
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c]:
+                    for d in DIR:
+                        nr, nc = r + d[0], c + d[1]
+                        if inBound(nr, nc) and grid[nr][nc]:
+                            union((r, c), (nr, nc))
+        
+        return max(size.values())
