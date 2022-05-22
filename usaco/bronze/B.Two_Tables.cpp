eebbb978a0c9bd1ed73c8730f8d9952c72939244
Yeabsira <dryeab@gmail.com>
2022-05-22 07:07:31 +0300

    -

---
+++ usaco/bronze/B.Two_Tables.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+void solve(){
+
+    int W, H;
+    cin >> W >> H;
+
+    int x1, y1, x2, y2;
+
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    int h, w;
+
+    cin >> w >> h;
+
+    
+
+}
+
+int main(){
+     
+     int t;
+     cin >> t;
+
+     while (t--){
+         solve();
+     }
+
+    return 0;
+}
