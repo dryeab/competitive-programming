d2ae6326c5548bc847557ed3c0d3603e05d1daae
Yeabsira Driba <72541965+dryeab@users.noreply.github.com>
2022-10-25 10:02:19 +0300

    Update template.py

--- template.py
+++ template.py
@@ -1,39 +1,16 @@
 import sys, threading
 
-# ------ Input Template --------
-
-input = sys.stdin.readline
-
-def iint():
-    """ int """
-    return int(input())
-
-def ilint():
-    """ List[int] """
-    return list(map(int,input().split()))
-
-def ilstr():
-    """ List[str] """
-    s = input()
-    return list(s[:len(s)-1])
-
-def imint():
-    """ Map[int] """
-    return map(int,input().split())
-
-found_recursion = False 
+input = lambda: sys.stdin.readline().strip()
 
 def main():
     pass
 
 if __name__ == '__main__':
     
-    if found_recursion:
-        sys.setrecursionlimit(1 << 30)
-        threading.stack_size(1 << 27)
+    sys.setrecursionlimit(1 << 30)
+    threading.stack_size(1 << 27)
 
-        main_thread = threading.Thread(target=main)
-        main_thread.start()
-        main_thread.join()
-    else:
-        main()
+    main_thread = threading.Thread(target=main)
+    main_thread.start()
+    main_thread.join()
+    
